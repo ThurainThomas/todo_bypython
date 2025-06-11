85307dd00176dee7b132b41242aec7c6e7e9a2dc
Thurain Win <thurainwin045@gmail.com>
2025-06-11 15:23:31 +0630

    RabbitMq_Step02.b: Work Queues of Rabbitmq that is used Message durability

--- send.py
+++ send.py
@@ -4,16 +4,14 @@ import sys
 connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 channel = connection.channel()
 
-channel.queue_declare(queue="hello")
+channel.queue_declare(queue="task_queue", durable=True)
 message = " ".join(sys.argv[1:]) or "Hello World!"
-channel.basic_publish(exchange="", routing_key="hello", body=message)
+channel.basic_publish(
+    exchange="",
+    routing_key="task_queue",
+    body=message,
+    properties=pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent),
+)
 print(f" [x] Sent {message}")
 
 connection.close()
-
-
-"""
-Forgotten acknowledgment
-sudo rabbitmqctl list_queues name messages_ready messages_unacknowledged
-sudo rabbitmqctl delete_queue task_queue
-"""
