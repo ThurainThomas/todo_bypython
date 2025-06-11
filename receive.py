e01fd317d71d3fbdfa7960fc26110f4918f1c1c0
Thurain Win <thurainwin045@gmail.com>
2025-06-11 15:30:46 +0630

    RabbitMq_Step02.b: Work Queues of Rabbitmq that is used Fair dispatch

--- receive.py
+++ receive.py
@@ -1,31 +1,22 @@
-import pika, sys, os
+#!/usr/bin/env python
+import pika
 import time
 
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+channel = connection.channel()
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
-    channel = connection.channel()
-
-    channel.queue_declare(queue="task_queue", durable=True)
-
-    def callback(ch, method, properties, body):
-        print(f" [x] Received {body.decode()}")
-        time.sleep(body.count(b"."))
-        print(" [x] Done")
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-
-    channel.basic_consume(queue="task_queue", on_message_callback=callback)
-
-    print(" [*] Waiting for messages. To exit press CTRL+C")
-    channel.start_consuming()
+channel.queue_declare(queue="task_queue", durable=True)
+print(" [*] Waiting for messages. To exit press CTRL+C")
 
 
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("Interrupted")
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+def callback(ch, method, properties, body):
+    print(f" [x] Received {body.decode()}")
+    time.sleep(body.count(b"."))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue="task_queue", on_message_callback=callback)
+
+channel.start_consuming()
--- send.py
+++ send.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
 import pika
 import sys
 
-connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
 channel = connection.channel()
 
 channel.queue_declare(queue="task_queue", durable=True)
+
 message = " ".join(sys.argv[1:]) or "Hello World!"
 channel.basic_publish(
     exchange="",
@@ -13,5 +15,4 @@ channel.basic_publish(
     properties=pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent),
 )
 print(f" [x] Sent {message}")
-
 connection.close()
